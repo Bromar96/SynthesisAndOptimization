794fc4d517e4d29acb0a1461768b17a49c14494c
Omar <gai.omar96@gmail.com>
2022-05-03 12:38:17 +0200

    LAB2 updated

---
+++ LAB2/explore_design.tcl
@@ -0,0 +1,5 @@
+puts "RUN THE POST SYNTHESIS STATIC ANALYSIS SCRIPT\n\n"
+source ./scripts/pt_analysis.tcl
+puts "\n\nRUN THE EXPLORE DESIGN SCRIPT\n\n"
+source ./scripts/info_cell2.tcl
+
--- LAB2/info_cell2.tcl
+++ LAB2/info_cell2.tcl
@@ -12,7 +12,7 @@ foreach_in_collection point_cell $ordered_collection {
    set cell_leakage [get_attribute $point_cell leakage_power]
    set cell_dynamic [get_attribute $point_cell dynamic_power]
    if { $cell_type == true } {
-        puts "$cell_name is combinational"
+        puts "$cell_name is combinational, leakage power is $cell_leakage"
         lappend cell_list_name $cell_name 
         lappend cell_list_ref $cell_ref
         lappend cell_list_area $cell_area
@@ -27,23 +27,22 @@ set chip_area 0
 foreach single_area $cell_list_area {
      set chip_area [expr {$single_area + $chip_area}] 
 }
-puts "Chip area: $chip_area"
 
 set chip_leakage 0
 foreach single_leakage $cell_list_leakage {
      set chip_leakage [expr {$single_leakage + $chip_leakage}] 
 }
-puts "Chip leakage power: $chip_leakage"
 
 set chip_dynamic 0
 foreach single_dyn $cell_list_dynamic {
      set chip_dynamic [expr {$single_dyn + $chip_dynamic}] 
 }
-puts "Chip dynamic power: $chip_dynamic"
 
 #To compare chip area
-#report_cell [get_cells -filter "is_combinational == true"]
-
+report_cell [get_cells -filter "is_combinational == true"] 
+puts "Compare with computed chip area $chip_area\n\n"
 #To compare chip power
-#report_power
+report_power
+puts "Compare with computed chip leakage power $chip_leakage"
+puts "Compare with computed chip dynamic power $chip_dynamic"
 #remember that dynamic power = InternalPower + SwitchingPower
