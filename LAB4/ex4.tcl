888c4682ff347fd0113cd93b35d500ade02d4355
Omar <gai.omar96@gmail.com>
2022-06-04 12:30:12 +0200

    LAB4 ex4 bug fixed

--- LAB4/ex4.tcl
+++ LAB4/ex4.tcl
@@ -14,10 +14,12 @@ proc ex4_proc {} {
     gets $fp line
     puts "Net \t\tStat_Prob\tTC\tTotSimPeriod"
     while {[gets $fp line] >=0 } {
-        regexp {\s+(\S+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)} $line match net toggle_count toggle_i time_at_one time_at_zero time_x 
+        regexp {\s+(.+\s?.+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)} $line match net toggle_count toggle_i time_at_one time_at_zero time_x 
         regsub {/test/u0/} $net "" Net
         regsub {\(} $Net "\[" Net
         regsub {\)} $Net "\]" Net
+        regsub {\\} $Net "" Net
+        regsub {\s} $Net "" Net
         set static_probability [expr double($time_at_one)/$total_sim]
         puts "$Net \t $static_probability \t\t $toggle_count \t $total_sim" 
         set_switching_activity $Net -static_probability $static_probability -toggle_rate $toggle_count -period $total_sim 
